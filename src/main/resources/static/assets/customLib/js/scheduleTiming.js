2f806db674d44a2b3bba066ed098c8fc7e11926e
nzepa74 <nzepa.ttpl@gmail.com>
2020-10-23 17:50:21 +0600

    grid update

--- src/main/resources/static/assets/customLib/js/scheduleTiming.js
+++ src/main/resources/static/assets/customLib/js/scheduleTiming.js
@@ -16,37 +16,29 @@ user = (function () {
             success: function (res) {
                 if (res.status === 1) {
                     let columnDef = [
+                        {data: "scheduleDetailId", class: "scheduleDetailId hidden"},
                         {
                             class: "align-middle", "mRender": function (data, type, row, meta) {
                                 return meta.row + 1;
                             }
                         },
                         {
-                            data: {
-                                scheduleDetailId: "scheduleDetailId",
-                                appointmentStatus: "appointmentStatus"
-                            },
+                            data: "appointmentStatus",
                             render: function (data) {
                                 let status = null;
-                                if (data.scheduleDetailId === 'A') {
-                                    status = '<input type="hidden" id="appointmentStatus" class="appointmentStatus" value="' + data.appointmentStatus + '">' +
-                                        '<i class="status-icon bg-blue spinner-grow"></i>' + "Approved";
+                                if (data === 'A') {
+                                    status = '<i class="status-icon bg-blue spinner-grow"></i>' + "Approved";
                                 }
                                 return status;
                             }
                         },
-                        {data: 'availableFrom', class: "align-middle"},
-                        {data: 'availableTo', class: "align-middle"},
-                        {
-                            "data": "scheduleDate", class: "align-middle",
-                            "mRender": function (data) {
-                                return formatAsDate(data);
-                            }
-                        },
+                        {data: 'availableFrom', class: "availableFrom align-middle"},
+                        {data: 'availableTo', class: "availableTo align-middle"},
+                        {data: "scheduleDate", class: "scheduleDate align-middle"},
                         {
                             "data": "null", class: "align-middle",
                             "mRender": function () {
-                                return '<a href="#" id="btnViewApplication" class="btn btn-primary btn-sm ml-3 d-none d-sm-inline-block" data-toggle="modal" data-target="#applicationDetailModal">View</a>';
+                                return '<a href="#" id="btnView" class="btn btn-primary btn-sm ml-3 d-none d-sm-inline-block" data-toggle="modal" data-target="#add_time_slot">View</a>';
                             }
                         }
                     ];
@@ -55,11 +47,32 @@ user = (function () {
                         , columns: columnDef
                         , destroy: true
                         , bSort: false
+                        , searching: false
+                        , info: false
+                        , paging: false
                     });
                 } else {
                     alert(res.text);
                 }
             }
+        });
+    }
+
+    function btnView() {
+        $('#timeSlotTableId tbody').on('click', 'tr #btnView', function () {
+            let row = $(this).closest('tr');
+            let selectedRow = row.addClass('selected');
+            let scheduleDetailId = selectedRow.find('.scheduleDetailId').text();
+            let availableFrom = selectedRow.find('.availableFrom').text();
+            let availableTo = selectedRow.find('.availableTo').text();
+            let scheduleDate = selectedRow.find('.scheduleDate').text();
+            selectedRow.removeClass('selected');
+            $('#scheduleDetailId').val(scheduleDetailId);
+            $('#availableFrom').val(availableFrom);
+            $('#availableTo').val(availableTo);
+            $('#scheduleDate').val(scheduleDate);
+            $(".modal-title").text(scheduleDate);
+            $('#btnSave').text('Update');
         });
     }
 
@@ -86,15 +99,18 @@ user = (function () {
         let masterDate = $("#masterDate").val();
         $("#scheduleDate").val(masterDate);
         $(".modal-title").text(masterDate);
+        $('#btnSave').text('Save');
     });
 
     return {
         getScheduleDetail: getScheduleDetail,
+        btnView: btnView,
         addSchedule: addSchedule
     }
 })();
 
 $(document).ready(function () {
     user.getScheduleDetail();
+    user.btnView();
     user.addSchedule();
 });
